c2c6da0a6b0fa7e6837ffe2ab3b5018ba1c02519
minesweepertkinter <166137146+minesweepertkinter@users.noreply.github.com>
2024-04-18 19:20:27 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,11 @@ from random import randint
 print("")
 print("Hi Rina, Hi Marieeeee can yall see this collaborative document?")
 
+print()
+print()
+print("YESSS FINALLY")
+print()
+print()
 
 print("ITS FINE COZ TTPD IS COMING OUT TWRRRR")
 
