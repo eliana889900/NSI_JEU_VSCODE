f0b67b51a4f8072f31350e7b54bef99361cff86b
eliana.889900 <eliana.mordchelles@gmail.com>
2024-04-18 19:03:30 +0200

    Added features

--- test.py
+++ test.py
@@ -3,9 +3,11 @@ from random import randint
 print("")
 print("Hi Rina, Hi Marieeeee can yall see this collaborative document?")
 
-print("NOOOO")
 
 print("ITS FINE COZ TTPD IS COMING OUT TWRRRR")
 
 
 print("Fan favourite track on ttpd will be track number", randint(1, 16) )
+
+print("I created a new branch")
+print("I created a new featureeee")
